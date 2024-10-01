3f94bbb8ac7960df5d4fb765b212b0cceed911f7
Parvish Rynal Mohan <rynalmp@gmail.com>
2024-10-01 18:12:30 +1200

    change

---
+++ wk10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+void printWelcome();
+int main(){
+
+void printWelcome();
+cout <<"Inside the main";
+
+
+return 0;
+}
+
+void printWelcome(){
+cout <<"The void function is executed to provide this message: "
+    <<"Bula Vinaka\n";
+
+};
